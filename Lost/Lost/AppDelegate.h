b4abcf4e817b731f0b6c0e10358c08ae9902cd8c
wch <1954860634@qq.com>
2016-09-29 20:10:15 +0800

    初始化项目

---
+++ Lost/Lost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lost
+//
+//  Created by moxuyou on 16/9/29.
+//  Copyright © 2016年 moxuyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lost/Lost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lost
+//
+//  Created by moxuyou on 16/9/29.
+//  Copyright © 2016年 moxuyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
